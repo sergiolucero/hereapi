fde6ff68855616cdf3942fe6b52c9d68ca57bb3a
Sergio Lucero <sergiolucero@gmail.com>
2018-05-21 17:03:40 -0400

    movin on

--- hereapi/__init__.py
+++ hereapi/__init__.py
@@ -1,1 +1,27 @@
+import os
+import requests
+import folium
+
 name = "hereapi"
+ID = os.getenv('HERE_ID')
+CODE = os.getenv('HERE_CODE')
+if ID is None:
+	raise Exception('ERROR: please define HERE_ID environment variable')
+	
+# home =[-33.406654,-70.572701]     # 085 LOS MILITARES / ALONSO DE CORDOVA
+heads = {'isoline': 'https://isoline.route.cit.api.here.com/routing/7.2/calculateisoline.json?'}
+URL_BASE = {'isoline': '{}app_id={}&app_code={}&mode=shortest;car;traffic:disabled&start=geo!{},{}&range={}&rangetype={}'}
+
+def get_isodata(home, range, type):
+	url = URL_BASE['isoline'].format(heads['isoline'], ID, CODE, home[0],home[1], range, type)
+	js = requests.get(url).json()['response']
+	# center = js['center']	# {'latitude': -33.406654, 'longitude': -70.572701}
+	iso = js['isoline']
+	comps = [(float(x.split(',')[0]),float(x.split(',')[1])) for x in iso[0]['component'][0]['shape']]
+	return comps
+
+def isoplot(home, locations, color, outputfile):
+	fm = folium.Map(location = home, zoom_start=13, tiles='CartoDBPositron')
+	folium.PolyLine(locations=locations,color=color).add_to(fm)
+	fm.save(outputfile)
+	print('map saved to %s' %outputfile)
--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setuptools.setup(
     author_email="sergiolucero@gmail.com",
     description="A small example package",
     long_description=long_description,
+	install_requires=['requests','folium'],
     long_description_content_type="text/markdown",
     url="https://github.com/sergiolucero/hereapi",
     packages=setuptools.find_packages(),
