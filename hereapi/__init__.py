bbe36a9255cac9b7fcf9345a830a471d87247ef0
Sergio Lucero <sergiolucero@gmail.com>
2018-05-21 19:04:18 -0400

    can now pass several ranges (and colors)

--- hereapi/__init__.py
+++ hereapi/__init__.py
@@ -13,15 +13,23 @@ heads = {'isoline': 'https://isoline.route.cit.api.here.com/routing/7.2/calculat
 URL_BASE = {'isoline': '{}app_id={}&app_code={}&mode=shortest;car;traffic:disabled&start=geo!{},{}&range={}&rangetype={}'}
 
 def get_isodata(home, range, type):
-	url = URL_BASE['isoline'].format(heads['isoline'], ID, CODE, home[0],home[1], range, type)
-	js = requests.get(url).json()['response']
-	# center = js['center']	# {'latitude': -33.406654, 'longitude': -70.572701}
-	iso = js['isoline']
-	comps = [(float(x.split(',')[0]),float(x.split(',')[1])) for x in iso[0]['component'][0]['shape']]
+	if isinstance(range, (list,array)):		# several values
+		comps = [get_isodata(home, rng, type) for rng in range]
+	else:
+		url = URL_BASE['isoline'].format(heads['isoline'], ID, CODE, home[0],home[1], range, type)
+		js = requests.get(url).json()['response']
+		# center = js['center']	# {'latitude': -33.406654, 'longitude': -70.572701}
+		iso = js['isoline']
+		comps = [(float(x.split(',')[0]),float(x.split(',')[1])) for x in iso[0]['component'][0]['shape']]
+		
 	return comps
 
 def isoplot(home, locations, color, outputfile):
 	fm = folium.Map(location = home, zoom_start=13, tiles='CartoDBPositron')
-	folium.PolyLine(locations=locations,color=color).add_to(fm)
+	if isinstance(locations[0][0],float):
+		folium.PolyLine(locations=locations,color=color).add_to(fm)
+	else:	# a list coming from above (or elsewhere), color is also a list
+		for loc,col in zip(locations,color):
+			folium.PolyLine(locations=loc,color=col).add_to(fm)
 	fm.save(outputfile)
 	print('map saved to %s' %outputfile)
